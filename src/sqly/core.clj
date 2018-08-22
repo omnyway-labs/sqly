f0e5e373c69975081b0edf06bfe03571a06a0b20
Ram Krishnan <kriyative@gmail.com>
2018-08-22 11:20:09 -0700

    Refactors emitter fns, adds dyn remappable idents, updates tests

--- src/sqly/core.clj
+++ src/sqly/core.clj
@@ -17,47 +17,78 @@
 
 (def infix-fns '#{and or = < <= > >= =/= not=})
 
-(defn emit-infix-op [op & args]
-  (str "("
-       (->> args
-            (map as-ident)
-            (str/join (str " " op " ")))
-       ")"))
+(defn emit-infix-op
+  ([v]
+   (emit-infix-op v {}))
+  ([[op & args] opts]
+   (str "("
+        (->> args
+             (map #(as-ident % opts))
+             (str/join (str " " op " ")))
+        ")")))
 
-(defn emit-function [fname & args]
-  (if (infix-fns fname)
-    (apply emit-infix-op fname args)
-    (let [args-str (->> args
-                        (map as-ident)
-                        (str/join ","))]
-      (str fname "(" args-str ")"))))
+(defn emit-function
+  ([v]
+   (emit-function v {}))
+  ([v opts]
+   (let [[fname & args] v]
+     (if (infix-fns fname)
+       (emit-infix-op v opts)
+       (let [args-str (->> args
+                           (map #(as-ident % opts))
+                           (str/join ", "))]
+         (str fname "(" args-str ")"))))))
 
-(defn as-ident [v]
-  (cond
-    (map? v)
-    (->> v
-         (map
-          (fn [[k v]]
-            (format "%s as %s" (as-ident v) (as-ident k))))
-         (str/join " "))
+(def ^:dynamic *remapped-idents*
+  {:not-nil "is not null"})
 
-    (list? v)
-    (apply emit-function v)
+(defn remap-ident [v]
+  (get *remapped-idents* v v))
 
-    (coll? v)
-    (->> v
-         (map as-ident)
-         (str/join " "))
+(defn emit-map
+  ([m]
+   (emit-map m {}))
+  ([m {:keys [separator]}]
+   (->> m
+        (map
+         (fn [[k v]]
+           (format "%s as %s" (as-ident v) (as-ident k))))
+        (str/join (or separator " ")))))
 
-    :else
-    (quote-iff (ident-str v))))
+(defn emit-coll
+  ([c]
+   (emit-coll c {}))
+  ([c {:keys [separator]}]
+   (->> c
+        (map as-ident)
+        (str/join (or separator " ")))))
+
+(defn as-ident
+  ([v]
+   (as-ident v {}))
+  ([v {:as opts :keys [emitters]}]
+   (cond
+     (map? v)
+     ((or (-> emitters :map) emit-map) v opts)
+
+     (list? v)
+     ((or (-> emitters :function) emit-function) v opts)
+
+     (coll? v)
+     ((or (-> emitters :coll) emit-coll) v opts)
+
+     :else
+     (-> v
+         (remap-ident)
+         (ident-str)
+         (quote-iff)))))
 
 (defn emit-idents
   ([idents]
-   (emit-idents idents ","))
-  ([idents separator]
+   (emit-idents idents {:separator ", "}))
+  ([idents {:as opts :keys [separator]}]
    (->> idents
-        (map as-ident)
+        (map #(as-ident % opts))
         (str/join separator))))
 
 (defn ensure-seq [v]
@@ -65,26 +96,38 @@
 
 (defn emit-clause
   ([clause content]
-   (emit-clause clause content ","))
-  ([clause content separator]
+   (emit-clause clause content {:separator ", "}))
+  ([clause content opts]
    (when content
-     (str clause " "
-          (emit-idents (ensure-seq content) separator)))))
+     (str/join " "
+               [clause
+                (emit-idents (ensure-seq content) opts)]))))
 
 (defn emit-clause-spaced [clause content]
-  (emit-clause clause content " "))
+  (emit-clause clause content {:separator " "}))
 
 (defn emit-order-by-clause [clause content]
   (->> content
-       (map #(emit-idents % " "))
-       (str/join ",")
+       (map #(emit-idents % {:separator " "}))
+       (str/join ", ")
        (str clause " ")))
+
+(defn emit-where-map
+  ([m]
+   (emit-where-map m {}))
+  ([m opts]
+   (emit-function (cons 'and (into [] m)) opts)))
+
+(defn emit-where-clause [clause content]
+  (emit-clause clause content
+               {:separator " "
+                :emitters {:map emit-where-map}}))
 
 (defn emit-select [{:keys [select from where order-by group-by limit]}]
   (->> [["select" select]
         ["from" from]
-        ["where" where emit-clause-spaced]
-        ["order by" order-by emit-order-by-clause]
+        ["where" where #'emit-where-clause]
+        ["order by" order-by #'emit-order-by-clause]
         ["group by" group-by]
         ["limit" limit]]
        (map
@@ -97,3 +140,9 @@
 
 (defmethod sql* :select [m]
   (emit-select (dissoc m :op)))
+
+(def sql-keywords #{:select :insert})
+
+(defn sql [m]
+  (when-let [op (some sql-keywords (keys m))]
+    (sql* (assoc m :op op))))
--- test/sqly/core_test.clj
+++ test/sqly/core_test.clj
@@ -14,28 +14,33 @@
          (sql/as-ident {:cnt '(count 1)}))))
 
 (def select-example
-  {:src    {:select   :*
-            :from     :zapbuy.cues
-            :where    '[(and [:zb-id "is not null"]
-                             [:event-type "is not null"])]
-            :order-by [[:name :asc]
-                       [:foo.datetime :desc]]
-            :group-by '[:context.event-type
-                        (month :datetime)
-                        (day :datetime)]}
-   :result (str
-            "select *"
-            " from \"zapbuy.cues\""
-            " where (zb_id is not null and event_type is not null)"
-            " order by name asc,\"foo.datetime\" desc"
-            " group by \"context.event_type\",month(datetime),day(datetime)")})
+  (str
+   "select *"
+   " from \"zapbuy.cues\""
+   " where (zb_id is not null and event_type is not null)"
+   " order by name asc, \"foo.datetime\" desc"
+   " group by \"context.event_type\", month(datetime), day(datetime)"))
 
-(deftest emit-select
-  (is (= (:result select-example)
-         (sql/emit-select (:src select-example)))))
-
-(deftest sql*
-  (is (= (:result select-example)
-         (sql/sql*
-          (assoc (:src select-example) :op :select)))))
-
+(deftest sql
+  (is (= select-example
+         (sql/sql
+          {:select   :*
+           :from     :zapbuy.cues
+           :where    '[(and [:zb-id :not-nil]
+                            [:event-type "is not null"])]
+           :order-by [[:name :asc]
+                      [:foo.datetime :desc]]
+           :group-by '[:context.event-type
+                       (month :datetime)
+                       (day :datetime)]})))
+  (is (= select-example
+         (sql/sql
+          {:select   :*
+           :from     :zapbuy.cues
+           :where    {:zb-id :not-nil
+                      :event-type :not-nil}
+           :order-by [[:name :asc]
+                      [:foo.datetime :desc]]
+           :group-by '[:context.event-type
+                       (month :datetime)
+                       (day :datetime)]}))))
